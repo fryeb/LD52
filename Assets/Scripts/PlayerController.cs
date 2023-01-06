5fe02c1ec146069e8cd9987275a2f5b247d824be
Ben Frye <ben.j.frye@gmail.com>
2023-01-07 10:59:36 +1100

    Add PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     public PlayerSettings settings;
+    public Transform engines;
 
-    // Start is called before the first frame update
+    private float speed = 0.0f;
+
+    private Rigidbody m_Rigidbody;
+
     void Start()
     {
-        
+	    m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+	    // Speed
+	    if (Input.GetKey(KeyCode.Space)) {
+		    speed += settings.GetAcceleration() * Time.fixedDeltaTime;
+	    } else {
+		    speed -= settings.GetDeceleration() * Time.fixedDeltaTime;
+	    }
+	    speed = Mathf.Clamp(speed, 0, settings.max_speed);
+
+	    m_Rigidbody.MovePosition(transform.position + transform.forward * speed);
+
+	    // Direction
+	    Vector3 euler = new Vector3(0, 0, 0);
+	    if (Input.GetKey(KeyCode.W)) {
+		    euler.x -= 360.0f / settings.pitch_time;
+	    }
+	    if (Input.GetKey(KeyCode.A)) {
+		    euler.y -= 360.0f / settings.yaw_time;
+	    }
+	    if (Input.GetKey(KeyCode.S)) {
+		    euler.x += 360.0f / settings.pitch_time;
+	    }
+	    if (Input.GetKey(KeyCode.D)) {
+		    euler.y += 360.0f / settings.yaw_time;
+	    }
+	    if (Input.GetKey(KeyCode.Q)) {
+		    euler.z += 360.0f / settings.roll_time;
+	    }
+	    if (Input.GetKey(KeyCode.E)) {
+		    euler.z -= 360.0f / settings.roll_time;
+	    }
+	    Quaternion delta_direction = Quaternion.Euler(euler * Time.fixedDeltaTime);
+	    m_Rigidbody.MoveRotation(m_Rigidbody.rotation * delta_direction);
+
+
+	    // Rotate Engines
+	    float revs_per_second = settings.max_revs * speed/settings.max_speed;
+	    float delta_degrees = revs_per_second * Time.fixedDeltaTime * 360;
+	    engines.Rotate(new Vector3(delta_degrees, 0, 0));
     }
 }
--- Assets/Scripts/PlayerSettings.cs
+++ Assets/Scripts/PlayerSettings.cs
@@ -6,5 +6,27 @@ using UnityEngine;
 public class PlayerSettings : ScriptableObject
 {
 	[Range(1.0f, 10.0f)]
-	public float speed = 1.0f;
+	public float max_speed = 1.0f;
+	[Range(0.01f, 10.0f)]
+	public float time_to_max_speed = 1.0f;
+	[Range(0.01f, 10.0f)]
+	public float time_to_rest = 1.0f;
+	[Range(1.0f, 10.0f)]
+	public float max_revs =  10.0f;
+
+	 // Seconds to full rotation
+	[Range(0.01f, 10.0f)]
+	public float pitch_time = 1.0f;
+	[Range(0.01f, 10.0f)]
+	public float yaw_time = 1.0f;
+	[Range(0.01f, 10.0f)]
+	public float roll_time = 1.0f;
+
+	public float GetAcceleration() {
+		return max_speed / time_to_max_speed;
+	}
+
+	public float GetDeceleration() {
+		return max_speed / time_to_rest;
+	}
 }
